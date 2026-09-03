a58b1449e2797607feaffaafd56b40f3ecc9eaf7
Roman Exemplarov <r.exemplarov@gmail.com>
2026-09-03 21:02:29 +1000

    fix: import setIcon from the obsidian module (it is not a global)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Plugin, ItemView, MarkdownRenderChild, MarkdownRenderer, Notice, PluginSettingTab, Setting } = require("obsidian");
+const { Plugin, ItemView, MarkdownRenderChild, MarkdownRenderer, Notice, PluginSettingTab, Setting, setIcon } = require("obsidian");
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
